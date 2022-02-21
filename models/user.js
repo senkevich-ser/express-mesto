01dbb3ba57687a321d3fe3af949d13ebb986900c
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-02-21 10:24:52 +0400

    add mail&password fields with validation by validator plagin

--- models/user.js
+++ models/user.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
   name: {
     type: String,
     required: true,
